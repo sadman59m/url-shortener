bfcc40d974d3f0dd70d92816804d2efbf5c2e253
Sadman Sakib <79523082+sadman59m@users.noreply.github.com>
2023-08-16 14:42:22 +0600

    Update Authentication.jsx

--- src/components/Authentication.jsx
+++ src/components/Authentication.jsx
@@ -6,7 +6,7 @@ import classes from "./Authentication.module.css";
 const Authentication = () => {
   return (
     <Card className={classes.main}>
-      <h1>Please, Provide an username to continue.</h1>
+      <h1>Please, Provide a username to continue.</h1>
       <div>
         <AuthForm />
       </div>
